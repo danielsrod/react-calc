1109d64a0028b4ea7dfc2dc4d7fc88b87e79a4ea
danielsrod <coeng.daniel@gmail.com>
2024-08-30 15:39:56 -0400

    add shadcn as ui compoents lib | setup layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 export const App = () => {
 	return (
 		<>
-			<h1>salve</h1>
+			<main className="w-full h-[100vh] bg-gradient-to-r from-fuchsia-600 to-purple-600 flex row justify-center items-center gap-3">
+				<div>history</div>
+				<div>calc</div>
+			</main>
 		</>
 	);
-}
+};
